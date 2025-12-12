23d5c141b57f524c58ba9a4041270a72cf33befd
MEDamine1910 <rouissmohamedamine@gmail.com>
2025-12-13 00:27:59 +0100

    .

---
+++ chatBot_telegram/chatBot_telegram/src/test/java/org/sid/enstbot/EnstBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.enstbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnstBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
